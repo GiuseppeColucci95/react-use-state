f2c358e6bb477b00662060cf559ed2cd31efa1ef
GiuseppeColucci95 <colucci539@gmail.com>
2025-03-25 17:14:01 +0100

    fix: simplify button click handler and improve active state management

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,14 @@
 export default function Button({ active, element, setActive }) {
 
   //logic here
-  function HandleClick(idToActivate) {
-    setActive(idToActivate);
+  function HandleClick() {
+    setActive(element.id);
   }
 
   //return here
   return (
     <>
-      <button className={`btn ${active === element.id ? "btn-warning" : "btn-primary"}`} onClick={() => {
-        HandleClick(element.id);
-      }}>{element.name}</button>
+      <button className={`btn ${active === element.id ? "btn-warning" : "btn-primary"}`} onClick={HandleClick}>{element.name}</button>
     </>
   );
 }
